e1aa873894faacfc9623accb97bc9867f81f1600
routersys <github.routersys@gmail.com>
2026-07-01 17:35:33 +0900

    アンカー座標のプレーン値モデルを追加

---
+++ LuaScript/Anchor/LuaAnchorPoint.cs
@@ -0,0 +1,11 @@
+namespace LuaScript.Anchor
+{
+    public sealed class LuaAnchorPoint
+    {
+        public string Group { get; set; } = string.Empty;
+        public int Index { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+    }
+}
