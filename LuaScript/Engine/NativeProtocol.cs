30c1a69b59693c4dce471ff45f6db0f9ca8f009f
routersys <github.routersys@gmail.com>
2026-07-01 17:52:18 +0900

    setanchorコールバック種別を追加

--- LuaScript/Engine/NativeProtocol.cs
+++ LuaScript/Engine/NativeProtocol.cs
@@ -55,6 +55,7 @@ namespace LuaScript.Engine
         public const int CbKindLoadText = 5;
         public const int CbKindLoadImage = 6;
         public const int CbKindLoadMovie = 7;
+        public const int CbKindSetAnchor = 8;
 
         public const int CbExist = 0;
         public const int CbX = 1;
