38c699e15463679ff7c6153209fb6aec1f928858
routersys <github.routersys@gmail.com>
2026-07-02 13:17:04 +0900

    IBufferCompositorのComposeを委譲可否を返すTryComposeへ変更

--- LuaScript/IBufferCompositor.cs
+++ LuaScript/IBufferCompositor.cs
@@ -2,6 +2,6 @@ namespace LuaScript
 {
     internal interface IBufferCompositor
     {
-        void Compose(byte[] dst, int dstW, int dstH, byte[] src, int srcW, int srcH, in DrawCommand command);
+        bool TryCompose(byte[] dst, int dstW, int dstH, byte[] src, int srcW, int srcH, in DrawCommand command);
     }
 }
