c83cca5418e40f07630cc50c0cca2d3703fb31e3
routersys <github.routersys@gmail.com>
2026-07-02 12:54:01 +0900

    SoftwareCompositorのComposeディスパッチのテストを追加

--- LuaScript.Tests/SoftwareCompositorTests.cs
+++ LuaScript.Tests/SoftwareCompositorTests.cs
@@ -62,6 +62,59 @@ namespace LuaScript.Tests
         }
 
         [Fact]
+        public void Compose_AffineCommand_MatchesDrawInto()
+        {
+            var src = Solid(1, 1, 10, 20, 30, 255);
+            var actual = new byte[3 * 3 * 4];
+            var expected = new byte[3 * 3 * 4];
+            var command = new DrawCommand(1.5, 1.5, 0d, 1d, 1d, 0d, Antialias: 0d);
+
+            new SoftwareCompositor().Compose(actual, 3, 3, src, 1, 1, command);
+            SoftwareCompositor.DrawInto(expected, 3, 3, src, 1, 1, 1.5, 1.5, 1d, 0d, 1d, linear: false);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Compose_PolyCommand_MatchesDrawPolyInto()
+        {
+            var src = Solid(2, 2, 40, 50, 60, 255);
+            var actual = new byte[2 * 2 * 4];
+            var expected = new byte[2 * 2 * 4];
+            double[] poly =
+            [
+                0, 0, 0, 2, 0, 0, 2, 2, 0, 0, 2, 0,
+                0, 0, 2, 0, 2, 2, 0, 2,
+                1,
+            ];
+            var command = new DrawCommand(0d, 0d, 0d, 1d, 1d, 0d, poly, Antialias: 0d);
+
+            new SoftwareCompositor().Compose(actual, 2, 2, src, 2, 2, command);
+            SoftwareCompositor.DrawPolyInto(expected, 2, 2, src, 2, 2, poly, 1d, linear: false);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Compose_AntialiasCommand_UsesLinearSampling()
+        {
+            var src = new byte[2 * 1 * 4];
+            src[3] = 255;
+            src[4] = 255;
+            src[5] = 255;
+            src[6] = 255;
+            src[7] = 255;
+            var actual = new byte[4 * 1 * 4];
+            var expected = new byte[4 * 1 * 4];
+            var command = new DrawCommand(2d, 0.5, 0d, 2d, 1d, 0d, Antialias: 1d);
+
+            new SoftwareCompositor().Compose(actual, 4, 1, src, 2, 1, command);
+            SoftwareCompositor.DrawInto(expected, 4, 1, src, 2, 1, 2d, 0.5, 2d, 0d, 1d, linear: true);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
         public void DrawPolyInto_IdentityAffine_CopiesSource()
         {
             var src = new byte[2 * 2 * 4];
