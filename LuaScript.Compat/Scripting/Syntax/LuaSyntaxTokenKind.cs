6f1eb733905450c995609d3f5e21aab0e2fa2806
routersys <github.routersys@gmail.com>
2026-07-03 21:30:19 +0900

    構文トークン種別を定義

---
+++ LuaScript.Compat/Scripting/Syntax/LuaSyntaxTokenKind.cs
@@ -0,0 +1,13 @@
+namespace LuaScript.Compat.Syntax
+{
+    internal enum LuaSyntaxTokenKind
+    {
+        Whitespace,
+        Newline,
+        Comment,
+        String,
+        Number,
+        Name,
+        Operator,
+    }
+}
